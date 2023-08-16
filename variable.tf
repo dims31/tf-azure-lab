9d2b089169de24d12d73c9fc594c8eb99483601f
Dimitri <35726773+dims31@users.noreply.github.com>
2023-08-16 10:57:16 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,9 @@
 variable users {
   type        = map(string)
+  description "Map to define Users : KEY = Display Name/VALUE = UserPrincipalName"
 }
 
 variable password {
     type = string
+    description = "Password applied to all users"
 }
